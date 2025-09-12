0b4845a794eae0b100b2020d08fd9ca351c65a95
GnJ Worldwide <gnjworldwide@outlook.com>
2025-09-12 15:27:09 +0530

    a

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 'use client'
 import React from 'react'
 import Banner from './Banner.jsx'
-import Introduction from './Introduction.jsx'
 import Client from './Client.jsx'
 
 export default function Home() {
