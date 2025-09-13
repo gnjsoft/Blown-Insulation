782967509da0a1284537a98d00f42d87854f3804
GnJ Worldwide <gnjworldwide@outlook.com>
2025-09-13 21:51:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Blown-Insulation/", // Important for GitHub Pages
+  base: "https://gnjsoft.com/", // Important for GitHub Pages
   plugins: [react(), tailwindcss()],
 })
