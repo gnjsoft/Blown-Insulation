075d4ae899cdea9c9f994e9f30d8a826e62dc799
GnJ Worldwide <gnjworldwide@outlook.com>
2025-09-14 15:29:25 +0530

    blown

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { BsYoutube } from 'react-icons/bs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Blown-Insulation/", // Important for GitHub Pages
+  base: "https://blowninsulationservices.com/", // Important for GitHub Pages
   plugins: [react(), tailwindcss()],
 })
